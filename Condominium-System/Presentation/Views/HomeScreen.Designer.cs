db82f3fdd33dc10accc224334addcd65d006f88e
“arieltotelegit config --global user.email arielasbel@hotmail.comgit config --global --listgit config --global user.name “arieltotele <arielasbel@hotmail.com>
2025-06-25 16:00:52 -0400

    Home Screen some advance implemented.

---
+++ Condominium-System/Presentation/Views/HomeScreen.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Condominium_System.Presentation.Views
+{
+    partial class HomeScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.MidnightBlue;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1202, 45);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(0, 44);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(135, 620);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(18, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Sistema de Condominios";
+            // 
+            // HomeScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1202, 759);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "HomeScreen";
+            Text = "HomeScreen";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+    }
+}
