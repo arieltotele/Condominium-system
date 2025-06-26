ecb97a0fc192e3166968a5ff003bc1b86d28df83
“arieltotelegit config --global user.email arielasbel@hotmail.comgit config --global --listgit config --global user.name “arieltotele <arielasbel@hotmail.com>
2025-06-26 16:03:04 -0400

    Screen to add serviced, added.

---
+++ Condominium-System/Presentation/Views/AddServiceScreen.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Condominium_System.Presentation.Views
+{
+    partial class AddServiceScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            HomeScreenLBLTitle = new Label();
+            checkBox6 = new CheckBox();
+            checkBox7 = new CheckBox();
+            checkBox8 = new CheckBox();
+            label2 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            label1 = new Label();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox9 = new CheckBox();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(217, 217, 217);
+            panel1.Controls.Add(HomeScreenLBLTitle);
+            panel1.Location = new Point(1, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(771, 64);
+            panel1.TabIndex = 0;
+            // 
+            // HomeScreenLBLTitle
+            // 
+            HomeScreenLBLTitle.AutoSize = true;
+            HomeScreenLBLTitle.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            HomeScreenLBLTitle.Location = new Point(11, 16);
+            HomeScreenLBLTitle.Name = "HomeScreenLBLTitle";
+            HomeScreenLBLTitle.Size = new Size(348, 32);
+            HomeScreenLBLTitle.TabIndex = 1;
+            HomeScreenLBLTitle.Text = "Añadir servicios a la vivienda";
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox6.Location = new Point(22, 212);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(148, 25);
+            checkBox6.TabIndex = 17;
+            checkBox6.Text = "Mantenimiento";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox7.Location = new Point(22, 165);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(148, 25);
+            checkBox7.TabIndex = 16;
+            checkBox7.Text = "Mantenimiento";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox8.Location = new Point(22, 120);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(107, 25);
+            checkBox8.TabIndex = 15;
+            checkBox8.Text = "Seguridad";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 25);
+            label2.TabIndex = 14;
+            label2.Text = "Esenciales:";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(240, 212);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(205, 25);
+            checkBox1.TabIndex = 21;
+            checkBox1.Text = "Actividades recreativas";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox2.Location = new Point(240, 165);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(158, 25);
+            checkBox2.TabIndex = 20;
+            checkBox2.Text = "Espacios sociales";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox3.Location = new Point(240, 120);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(158, 25);
+            checkBox3.TabIndex = 19;
+            checkBox3.Text = "Areas recreativas";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(230, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(125, 25);
+            label1.TabIndex = 18;
+            label1.Text = "Comunitarios";
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox4.Location = new Point(519, 212);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(247, 25);
+            checkBox4.TabIndex = 25;
+            checkBox4.Text = "Estacionamiento para visitas";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox5.Location = new Point(519, 165);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(205, 25);
+            checkBox5.TabIndex = 24;
+            checkBox5.Text = "Recepcion de paquetes";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox9.Location = new Point(519, 120);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(113, 25);
+            checkBox9.TabIndex = 23;
+            checkBox9.Text = "Lavanderia";
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(509, 79);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 25);
+            label3.TabIndex = 22;
+            label3.Text = "Convivencia";
+            // 
+            // AddServiceScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(772, 362);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox9);
+            Controls.Add(label3);
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox3);
+            Controls.Add(label1);
+            Controls.Add(checkBox6);
+            Controls.Add(checkBox7);
+            Controls.Add(checkBox8);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Name = "AddServiceScreen";
+            Text = "AddServiceScreen";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label HomeScreenLBLTitle;
+        private CheckBox checkBox6;
+        private CheckBox checkBox7;
+        private CheckBox checkBox8;
+        private Label label2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Label label1;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox9;
+        private Label label3;
+    }
+}
