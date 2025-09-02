b21a26cb78b5410b4b07717e3eafe6f6387a38e7
“arieltotelegit config --global user.email arielasbel@hotmail.comgit config --global --listgit config --global user.name “arieltotele <arielasbel@hotmail.com>
2025-09-02 11:23:13 -0400

    ER diagram updated.

--- Condominium-System/Data/Entities/Receipt.cs
+++ Condominium-System/Data/Entities/Receipt.cs
@@ -15,9 +15,9 @@ namespace Condominium_System.Data.Entities
         public string? Detail { get; set; }
         public string? Status { get; set; }
 
-        public int LateFee { get; set; } = 0; // Monto de mora
-        public bool LateFeeApplied { get; set; } = false; // Si ya se aplicó la mora
-        public DateTime? LateFeeAppliedDate { get; set; } // Cuando se aplicó la mora
+        public int LateFee { get; set; } = 0;
+        public bool LateFeeApplied { get; set; } = false;
+        public DateTime? LateFeeAppliedDate { get; set; }
 
         public int TenantId { get; set; }
         public virtual Tenant Tenant { get; set; }
